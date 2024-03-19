cbc4b17f93fa06b7126677c8f344238ef544c6dc
Sawa <sovalapati1133@gmail.com>
2024-03-19 23:37:41 +0500

    Change json converter

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -10,30 +9,18 @@ import (
 	"time"
 )
 
-type Location struct {
-	City      string `json:"city"`
-	Continent string `json:"continent"`
-}
-
-func compressJson(jsonData string) string {
-	compressedData := &bytes.Buffer{}
-
-	if err := json.Compact(compressedData, []byte(jsonData)); err != nil {
-		panic(err)
-	}
-
-	return compressedData.String()
-}
-func jsonToMap(jsonData []byte) map[string]interface{} {
-	var result map[string]interface{}
+func jsonToMap(jsonData []byte, url string) interface{} {
+	var result interface{}
 	err := json.Unmarshal(jsonData, &result)
 	if err != nil {
-		fmt.Print("Error")
+		println(url)
+		println(err)
+		return string(jsonData)
 	}
 	return result
 }
 
-func sendPostRequest(url string, wg *sync.WaitGroup, ch chan map[string]interface{}) {
+func sendGetRequest(url string, wg *sync.WaitGroup, ch chan map[string]interface{}) {
 	defer wg.Done()
 
 	startTime := time.Now()
@@ -53,9 +40,11 @@ func sendPostRequest(url string, wg *sync.WaitGroup, ch chan map[string]interfac
 		return
 	}
 
+	println(resp.S)
+
 	ch <- map[string]interface{}{
-		"duration": time.Since(startTime),
-		"body":     jsonToMap(body),
+		"duration": time.Since(startTime) / 1e6,
+		"body":     jsonToMap(body, url),
 		"url":      url,
 	}
 }
@@ -63,28 +52,31 @@ func sendPostRequest(url string, wg *sync.WaitGroup, ch chan map[string]interfac
 func main() {
 	urls := []string{
 		// "https://cat-fact.herokuapp.com/facts",
-		// "https://cataas2.com/cat",
+		"https://cataas.com/cat",
 		// "https://api.bigdatacloud.net/data/reverse-geocode-with-timezone?latitude=52.2250321&longitude=104.9633463&localityLanguage=ru",
 		// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=43.2250321&longitude=76.9633463&localityLanguage=ru",
-		"https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=51.2250321&longitude=71.9633463&localityLanguage=ru",
+		// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=51.2250321&longitude=71.9633463&localityLanguage=ru",
 	}
 
 	var wg sync.WaitGroup
+	var returnData []map[string]interface{}
 	ch := make(chan map[string]interface{}, len(urls))
 
 	for _, url := range urls {
 		wg.Add(1)
-		go sendPostRequest(url, &wg, ch)
+		go sendGetRequest(url, &wg, ch)
 	}
 
 	wg.Wait()
 	close(ch)
 
 	for response := range ch {
-		respData, err := json.Marshal(response)
-		if err != nil {
-			fmt.Printf("ERROR")
-		}
-		fmt.Println(string(respData))
+		returnData = append(returnData, response)
 	}
+
+	// respData, err := json.Marshal(returnData)
+	// if err != nil {
+	// 	fmt.Printf("ERROR")
+	// }
+	// fmt.Println(string(respData))
 }
